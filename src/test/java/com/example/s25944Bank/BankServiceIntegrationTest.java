1a7597df56af0851709413932d353383d04a6289
KarolSielski02 <mrkarlos2002@gmail.com>
2023-01-21 19:13:18 +0100

    Update BankServiceIntegrationTest.java

--- src/test/java/com/example/s25944Bank/BankServiceIntegrationTest.java
+++ src/test/java/com/example/s25944Bank/BankServiceIntegrationTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,7 +27,6 @@ public class BankServiceIntegrationTest {
         User result = bankService.createNewUser(1,100);
         assertThat(result).isNull();
     }
-
     @Test
     void BankService_shouldNotCreateUserWhenIdIsLessThan0(){
         when(userStorage.getUserList()).thenReturn(List.of(new User(1, 100)));
@@ -34,4 +34,25 @@ public class BankServiceIntegrationTest {
         User result = bankService.createNewUser(-10,100);
         assertThat(result).isNull();
     }
+
+    @Test
+    void CreatingTransaction_transfer(){
+        List<Transaction> mockList = new ArrayList<>();
+        when(userStorage.getUserList()).thenReturn(List.of(new User(1, 100)));
+        when(transactionStorage.getTransactionList()).thenReturn(mockList);
+        var result = bankService.transfer(1, 10).toString();
+        String testTransaction = new Transaction(10, 1, TransactionStatus.ACCEPTED, -10, 90).toString();
+        assertThat(result).isEqualTo(testTransaction);
+    }
+
+    @Test
+    void CreatingTransaction_deposit(){
+        List<Transaction> mockList = new ArrayList<>();
+        when(userStorage.getUserList()).thenReturn(List.of(new User(1, 100)));
+        when(transactionStorage.getTransactionList()).thenReturn(mockList);
+        var result = bankService.deposit(1, 10).toString();
+        String testTransaction = new Transaction(11, 1, TransactionStatus.ACCEPTED, 10, 110).toString();
+        assertThat(result).isEqualTo(testTransaction);
+    }
+
 }
