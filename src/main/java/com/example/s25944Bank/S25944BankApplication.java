a82c324cbda97fe9ea031642811e955524a63538
KarolSielski02 <mrkarlos2002@gmail.com>
2023-01-21 19:20:37 +0100

    Changes to integration tests

--- src/main/java/com/example/s25944Bank/S25944BankApplication.java
+++ src/main/java/com/example/s25944Bank/S25944BankApplication.java
@@ -6,17 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class S25944BankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(S25944BankApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(S25944BankApplication.class, args);
+    }
 
-	public S25944BankApplication(BankService bankService) {
-		System.out.println(bankService.createNewUser(3, 2500.30));
-		System.out.println("Transfer");
-		System.out.println(bankService.transfer(1,20).getSaldoAfter());
-		System.out.println(bankService.deposit(1,20).getSaldoAfter());
-		System.out.println(bankService.getUserInfo(1));
+    public S25944BankApplication(BankService bankService) {
+//        System.out.println(bankService.createNewUser(3, 2500.30));
+//        System.out.println("Transfer");
+//        System.out.println(bankService.transfer(1, 20).getSaldoAfter());
+//        System.out.println(bankService.deposit(1, 20).getSaldoAfter());
+//        System.out.println(bankService.getUserInfo(1));
 
-	}
+    }
 
 }
