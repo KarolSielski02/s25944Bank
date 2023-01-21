86341db1b6d90b0f4884c3942e93dbab15e4adf9
KarolSielski02 <mrkarlos2002@gmail.com>
2023-01-21 19:28:41 +0100

    Integration Tests work :)

--- src/test/java/com/example/s25944Bank/BankServiceIntegrationTest.java
+++ src/test/java/com/example/s25944Bank/BankServiceIntegrationTest.java
@@ -34,6 +34,15 @@ public class BankServiceIntegrationTest {
         User result = bankService.createNewUser(-10,100);
         assertThat(result).isNull();
     }
+    @Test
+    void CreatingUser_createNewUser(){
+        List<User> mockList = new ArrayList<>();
+        mockList.add(new User(1, 100));
+        when(userStorage.getUserList()).thenReturn(mockList);
+        var result = bankService.createNewUser(2, 100).toString();
+        String mockUser = new User(2,100).toString();
+        assertThat(result).isEqualTo(mockUser);
+    }
 
     @Test
     void CreatingTransaction_transfer(){
@@ -46,6 +55,16 @@ public class BankServiceIntegrationTest {
     }
 
     @Test
+    void WhatIf_idIsZero_transfer(){
+        List<Transaction> mockList = new ArrayList<>();
+        when(userStorage.getUserList()).thenReturn(List.of(new User(0, 100)));
+        when(transactionStorage.getTransactionList()).thenReturn(mockList);
+        var result = bankService.transfer(1, 10);
+//        String testTransaction = new Transaction(10, 1, TransactionStatus.ACCEPTED, -10, 90).toString();
+        assertThat(result).isEqualTo(null);
+    }
+
+    @Test
     void CreatingTransaction_deposit(){
         List<Transaction> mockList = new ArrayList<>();
         when(userStorage.getUserList()).thenReturn(List.of(new User(1, 100)));
@@ -54,5 +73,17 @@ public class BankServiceIntegrationTest {
         String testTransaction = new Transaction(11, 1, TransactionStatus.ACCEPTED, 10, 110).toString();
         assertThat(result).isEqualTo(testTransaction);
     }
+
+    @Test
+    void WhatIf_idIsZero_deposit(){
+        List<Transaction> mockList = new ArrayList<>();
+        when(userStorage.getUserList()).thenReturn(List.of(new User(1, 100)));
+        when(transactionStorage.getTransactionList()).thenReturn(mockList);
+        var result = bankService.deposit(0, 10);
+//        String testTransaction = new Transaction(10, 1, TransactionStatus.ACCEPTED, -10, 90).toString();
+        assertThat(result).isEqualTo(null);
+    }
+
+
 
 }
--- src/test/java/com/example/s25944Bank/BankServiceMockTest.java
+++ src/test/java/com/example/s25944Bank/BankServiceMockTest.java
@@ -41,6 +41,15 @@ public class BankServiceMockTest {
         User result = bankService.createNewUser(-10,100);
         assertThat(result).isNull();
     }
+    @Test
+    void CreatingUser_createNewUser(){
+        List<User> mockList = new ArrayList<>();
+        mockList.add(new User(1, 100));
+        when(userStorage.getUserList()).thenReturn(mockList);
+        var result = bankService.createNewUser(2, 100).toString();
+        String mockUser = new User(2,100).toString();
+        assertThat(result).isEqualTo(mockUser);
+    }
 
     @Test
     void CreatingTransaction_transfer(){
@@ -61,5 +70,26 @@ public class BankServiceMockTest {
         String testTransaction = new Transaction(11, 1, TransactionStatus.ACCEPTED, 10, 110).toString();
         assertThat(result).isEqualTo(testTransaction);
     }
+
+    @Test
+    void WhatIf_idIsZero_transfer(){
+        List<Transaction> mockList = new ArrayList<>();
+        when(userStorage.getUserList()).thenReturn(List.of(new User(0, 100)));
+        when(transactionStorage.getTransactionList()).thenReturn(mockList);
+        var result = bankService.transfer(1, 10);
+//        String testTransaction = new Transaction(10, 1, TransactionStatus.ACCEPTED, -10, 90).toString();
+        assertThat(result).isEqualTo(null);
+    }
+
+    @Test
+    void WhatIf_idIsZero_deposit(){
+        List<Transaction> mockList = new ArrayList<>();
+        when(userStorage.getUserList()).thenReturn(List.of(new User(1, 100)));
+        when(transactionStorage.getTransactionList()).thenReturn(mockList);
+        var result = bankService.deposit(0, 10);
+//        String testTransaction = new Transaction(10, 1, TransactionStatus.ACCEPTED, -10, 90).toString();
+        assertThat(result).isEqualTo(null);
+    }
+
 
 }
